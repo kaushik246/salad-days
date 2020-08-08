c8132e3a662fec68b7670ecf35eb4c0cda6e8e65
KrishnaBharathReddy <chkbr8021@gmail.com>
2020-08-08 16:37:47 +0530

    Created generic components for Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
-
 import './styles.scss'
-
 import Items from './components/Items'
-
-const mapStateToProps = (state) => {
-  return {
-    user: state.app.user
-  }
-}
+import Packaging from './components/Packaging'
 
 export class App extends Component {
   render() {
     return (
       <div>
+        <Packaging/>
         <Items/>
       </div>
     )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.app.user
   }
 }
 
